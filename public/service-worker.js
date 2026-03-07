0d6b4a32c15ffd683987f3220a9c3a023e748e1d
Jeganvishnu <jj9025696449@gmail.com>
2026-03-07 18:49:02 +0530

    Fix PWABuilder errors adding full manifest attributes and service worker caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,27 @@
+const CACHE_NAME = "safehire-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/SafeHire.png"
+];
+
 self.addEventListener("install", event => {
-    console.log("Service Worker Installed");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
 });
